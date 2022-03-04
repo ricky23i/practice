85d8c02e4f3d8ffb3b1963bb8b9441dbea7ebcb4
ricky23i <rduenas2@illinois.edu>
2022-03-04 13:20:15 -0600

    complete

--- py/src/Main.java
+++ py/src/Main.java
@@ -1,19 +1,46 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Main {
-public static void main(String[] args) {
-	String a ="richardduenas";
-	char[] b= a.toCharArray();
-	Map<Character, Integer> counter= new HashMap<>();
-		for(char c:b) {
-		if(counter.containsKey(c))
-			counter.put(c, counter.get(c)+1);
-		else
-			counter.put(c, 1);
-	}
+public class Py {
 	
-	System.out.println(counter);
+public static void main(String[] args) {
+	
+	int j=1; 
+	int r=0;
+	int current= 0;
+	for(int t= 1;j<501;t++) {
+		if(isTriangularNumber(t)) {
+			current=t;
+		for(int i=1; i<current/2;i++) {
+			System.out.println(current+": div"+j);
+			if(current%i==0)
+				j++;
+			if(j>500) {
+				System.out.println("found");
+				r=current;
+				break;
+			}
+		}
+		}
+		if(j>500)
+			break;
+		else
+		j=1;
+	}
+		
+		
+		System.out.println(r);
 	
 }
+public static boolean isTriangularNumber( int x)
+{
+    if (x < 0)
+        return false;
+
+    int n = (int) Math.sqrt(2 * x);
+    return n * (n + 1) / 2 == x;
+}
+
 }
